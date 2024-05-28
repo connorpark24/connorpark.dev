99916e33ca5f0153440ccca2580119eb42c2c8b0
Connor Park <connorpark024@gmail.com>
2024-05-27 22:11:46 -0500

    add project links

--- src/app/projects/projectData.tsx
+++ src/app/projects/projectData.tsx
@@ -13,21 +13,21 @@ type Project = {
 
 const projectData: Project[] = [
   {
-    link: "https://google.com",
+    link: "https://github.com/connorpark24/upround-platform",
     title: "UpRound Ventures Platform",
     description:
       "A club platform for managing startup sourcing and deal flow for UpRound Ventures.",
     icon: <ComputerDesktopIcon className="w-5 h-5 dark:text-white" />,
   },
   {
-    link: "https://google.com",
-    title: "Winston",
+    link: "https://github.com/connorpark24/winston",
+    title: "Winston - AI Quiz Generator",
     description:
       "An online quiz generator leveraging AI to create custom practice questions from lectures.",
     icon: <ComputerDesktopIcon className="w-5 h-5 dark:text-white" />,
   },
   {
-    link: "https://google.com",
+    link: "https://marketplace.visualstudio.com/items?itemName=connorpark024.college-theme-generator",
     title: "College Theme VSCode Extension",
     description:
       "A VSCode extension providing theme configurations inspired by college colors.",
