51f3c5344bb0f4efc88224d6454e24db91bab1f0
Connor Park <connorpark024@gmail.com>
2024-11-05 22:12:28 -0500

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
@@ -17,20 +18,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.className} bg-neutral-50 dark:bg-black flex flex-col h-full w-screen`}
-      >
-        <div className="flex justify-center fixed inset-0 lg:px-36 sm:px-16">
-          <div className="bg-white dark:bg-neutral-900 w-full max-w-7xl border-x-[1px] border-neutral-200 dark:border-neutral-800"></div>
-        </div>
+      <html lang="en">
+        <body
+          className={`${inter.className} bg-neutral-50 dark:bg-black flex flex-col h-full w-screen`}
+        >
+          <div className="flex justify-center fixed inset-0 lg:px-36 sm:px-16">
+            <div className="bg-white dark:bg-neutral-900 w-full max-w-7xl border-x-[1px] border-neutral-200 dark:border-neutral-800"></div>
+          </div>
 
-        <div className="relative lg:px-36 sm:px-16 max-w-[98rem] mx-auto w-full">
-          <Navbar />
-          <div className="py-24 sm:px-24 px-12">{children}</div>
-          <Footer />
-        </div>
-      </body>
-    </html>
+          <div className="relative lg:px-36 sm:px-16 max-w-[98rem] mx-auto w-full">
+            <Analytics />
+            <Navbar />
+            <div className="py-24 sm:px-24 px-12">{children}</div>
+            <Footer />
+          </div>
+        </body>
+      </html>
   );
 }
