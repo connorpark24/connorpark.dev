2b786c47b3078f7a4a4571fddfbbd32138fc65b0
Connor Park <connorpark024@gmail.com>
2025-07-11 09:19:25 -0700

    clean up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,43 +7,14 @@ import ExperienceCard from "@/components/ExperienceCard";
 export default function Home() {
   return (
     <div className="flex flex-col w-full">
-      <div className="relative inline-block dark:bg-neutral-900 bg-white border-gray-100 rounded-full sm:w-40 sm:h-40 w-32 h-32 shadow-2xl mb-12 dark:shadow-blue-600 shadow-blue-200">
-        <Image
-          src={headshot}
-          alt="Headshot"
-          className="rounded-full h-full w-full"
-        />
-      </div>
-
-      <div className="text-5xl font-medium mb-4 dark:text-white tracking-tight">
-        Connor Park
-      </div>
-      <div className="text-lg mb-4 dark:text-white ">
+      <div className="text-5xl mb-4 tracking-tight">Connor Park</div>
+      <div className="text-xl text-muted-foreground mb-4 ">
         Student at the University of Michigan
       </div>
 
       <ContactIcons />
 
-      <div className="flex xl:flex-row flex-col gap-12">
-        <div className="text-base flex flex-col gap-y-8 xl:w-3/5 leading-7 text-zinc-600 dark:text-zinc-400">
-          <p>
-            I&apos;m a rising sophomore studying Computer Science at the
-            University of Michigan, also working toward minors in Mathematics
-            and Business. At school, I&apos;m involved with{" "}
-            <Link className="font-semibold" href="https://v1michigan.com/">
-              V1 Michigan
-            </Link>{" "}
-            and{" "}
-            <Link className="font-semibold" href="https://www.upround.vc/">
-              UpRound Ventures.
-            </Link>
-          </p>
-          <p>Feel free to reach out!</p>
-        </div>
-        <div className="xl:w-2/5 flex flex-col gap-y-8">
-          <ExperienceCard />
-        </div>
-      </div>
+      <p className="text-lg text-muted-foreground mt-24">Building Skope</p>
     </div>
   );
 }
--- src/components/ContactIcons.tsx
+++ src/components/ContactIcons.tsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 
 export default function ContactIcons() {
   return (
-    <div className="flex flex-row md:gap-x-4 gap-x-3 mb-8 fill-neutral-700 dark:fill-white">
+    <div className="flex flex-row md:gap-x-3 gap-x-2 fill-muted-foreground dark:fill-white">
       <Link
         className=" transform transition-transform duration-100 hover:scale-[115%]"
         href="https://www.linkedin.com/in/connor-park24"
       >
         <svg
           viewBox="0 0 24 24"
-          className="lg:h-7 lg:w-7 w-6 h-6 fill-neutral-700 dark:fill-white"
+          className="lg:h-7 lg:w-7 w-6 h-6 fill-muted-foreground dark:fill-white"
         >
           <path d="M18.335 18.339H15.67v-4.177c0-.996-.02-2.278-1.39-2.278-1.389 0-1.601 1.084-1.601 2.205v4.25h-2.666V9.75h2.56v1.17h.035c.358-.674 1.228-1.387 2.528-1.387 2.7 0 3.2 1.778 3.2 4.091v4.715zM7.003 8.575a1.546 1.546 0 01-1.548-1.549 1.548 1.548 0 111.547 1.549zm1.336 9.764H5.666V9.75H8.34v8.589zM19.67 3H4.329C3.593 3 3 3.58 3 4.297v15.406C3 20.42 3.594 21 4.328 21h15.338C20.4 21 21 20.42 21 19.703V4.297C21 3.58 20.4 3 19.666 3h.003z" />
         </svg>
@@ -21,7 +21,7 @@ export default function ContactIcons() {
         <svg
           viewBox="0 0 24 24"
           aria-hidden="true"
-          className="lg:h-7 lg:w-7 w-6 h-6 fill-neutral-700 dark:fill-white"
+          className="lg:h-7 lg:w-7 w-6 h-6 fill-muted-foreground dark:fill-white"
         >
           <path
             fillRule="evenodd"
