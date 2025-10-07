5722bfcc436607b98d531ee70a594a421767e401
Connor Park <connorpark024@gmail.com>
2025-10-07 15:26:12 -0700

    minor updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,83 @@
 import Link from "next/link";
 import Image from "next/image";
+
+const galleryImages = [
+  {
+    src: "/gallery/ollie-tongue.jpeg",
+    alt: "ollie-tongue",
+    colSpan: 3,
+    message: "Ollie",
+  },
+  {
+    src: "/gallery/rosebowl.jpeg",
+    alt: "rosebowl",
+    colSpan: 5,
+    message: "Rose Bowl 2024 - Pasadena, CA",
+  },
+  { src: "/gallery/yc-s25.jpeg", alt: "yc-s25", colSpan: 4, message: "YC S25" },
+  {
+    src: "/gallery/daeho.jpeg",
+    alt: "daeho",
+    colSpan: 3,
+    message: "San Francisco, CA",
+  },
+  {
+    src: "/gallery/ribeyes.jpeg",
+    alt: "ribeyes",
+    colSpan: 3,
+    message: "Ribeyes",
+  },
+  {
+    src: "/gallery/udon.jpeg",
+    alt: "udon",
+    colSpan: 3,
+    message: "Tokyo, Japan",
+  },
+  {
+    src: "/gallery/tsujiki.jpeg",
+    alt: "tsujiki",
+    colSpan: 3,
+    message: "Tokyo, Japan",
+  },
+  { src: "/gallery/alaska.jpeg", alt: "alaska", colSpan: 5, message: "Alaska" },
+  {
+    src: "/gallery/florence.jpeg",
+    alt: "florence",
+    colSpan: 4,
+    message: "Florence, Italy",
+  },
+  {
+    src: "/gallery/costarica-tree.jpeg",
+    alt: "costarica-tree",
+    colSpan: 3,
+    message: "La Fortuna, Costa Rica",
+  },
+  {
+    src: "/gallery/rome-pasta.jpeg",
+    alt: "rome-pasta",
+    colSpan: 3,
+    message: "Rome, Italy",
+  },
+  {
+    src: "/gallery/republique.jpeg",
+    alt: "republique",
+    colSpan: 3,
+    message: "Los Angeles, CA",
+  },
+  {
+    src: "/gallery/nubiani.jpeg",
+    alt: "nubiani",
+    colSpan: 3,
+    message: "New York City, NY",
+  },
+  {
+    src: "/gallery/lindustrie.jpeg",
+    alt: "lindustrie",
+    colSpan: 3,
+    message: "New York City, NY",
+  },
+];
+
 export default function Home() {
   return (
     <div className="h-screen w-screen bg-background flex flex-col items-center">
@@ -27,7 +105,8 @@ export default function Home() {
           <p className=" text-foreground ">About</p>
           <p className=" text-muted-foreground mt-0.5">
             I previously studied Computer Science at the University of Michigan,
-            Ann Arbor. I enjoy trying new foods and watching Michigan Football.
+            Ann Arbor. I enjoy trying new restaurants, playing golf, and
+            watching Michigan Football.
           </p>
         </div>
         <div className="flex flex-col mt-12">
@@ -36,144 +115,20 @@ export default function Home() {
         </div>
         <div className="flex flex-col mt-32 mb-24">
           <div className="grid grid-cols-12 gap-2">
-            {/* ROW 1 */}
-            <div className="col-span-3  h-24 sm:h-32 lg:h-40 overflow-hidden rounded-md">
-              <Image
-                src="/gallery/ollie-tongue.jpeg"
-                alt="ollie-tongue"
-                className="w-full h-full object-cover"
-                width={500}
-                height={500}
-              />
-            </div>
-
-            <div className="col-span-5 h-24 sm:h-32 lg:h-40 overflow-hidden rounded-md">
-              <Image
-                src="/gallery/rosebowl.jpeg"
-                alt="rosebowl"
-                className="w-full h-full object-cover"
-                width={500}
-                height={500}
-              />
-            </div>
-
-            <div className="col-span-4  h-24 sm:h-32 lg:h-40  overflow-hidden rounded-md">
-              <Image
-                src="/gallery/yc-s25.jpeg"
-                alt="yc-s25"
-                className="w-full h-full object-cover"
-                width={500}
-                height={500}
-              />
-            </div>
-            {/* ROW 2 */}
-            <div className="col-span-3 h-24 sm:h-32 lg:h-40  overflow-hidden rounded-md">
-              <Image
-                src="/gallery/daeho.jpeg"
-                alt="daeho"
-                className="w-full h-full object-cover"
-                width={500}
-                height={500}
-              />
-            </div>
-
-            <div className="col-span-3 h-24 sm:h-32 lg:h-40  overflow-hidden rounded-md">
-              <Image
-                src="/gallery/ribeyes.jpeg"
-                alt="ribeyes"
-                className="w-full h-full object-cover"
-                width={500}
-                height={500}
-              />
-            </div>
-
-            <div className="col-span-3 h-24 sm:h-32 lg:h-40  overflow-hidden rounded-md">
-              <Image
-                src="/gallery/udon.jpeg"
-                alt="udon"
-                className="w-full h-full object-cover"
-                width={500}
-                height={500}
-              />
-            </div>
-            <div className="col-span-3 h-24 sm:h-32 lg:h-40  overflow-hidden rounded-md">
-              <Image
-                src="/gallery/tsujiki.jpeg"
-                alt="tsujiki"
-                className="w-full h-full object-cover"
-                width={500}
-                height={500}
-              />
-            </div>
-
-            {/* ROW 3 */}
-            <div className="col-span-5  h-24 sm:h-32 lg:h-40  overflow-hidden rounded-md">
-              <Image
-                src="/gallery/alaska.jpeg"
-                alt="alaska"
-                className="w-full h-full object-cover"
-                width={500}
-                height={500}
-              />
-            </div>
-
-            <div className="col-span-4  h-24 sm:h-32 lg:h-40  overflow-hidden rounded-md">
-              <Image
-                src="/gallery/florence.jpeg"
-                alt="florence"
-                className="w-full h-full object-cover"
-                width={500}
-                height={500}
-              />
-            </div>
-
-            <div className="col-span-3 h-24 sm:h-32 lg:h-40  overflow-hidden rounded-md">
-              <Image
-                src="/gallery/costarica-tree.jpeg"
-                alt="costarica-tree"
-                className="w-full h-full object-cover"
-                width={500}
-                height={500}
-              />
-            </div>
-
-            {/* ROW 4 */}
-            <div className="col-span-3 h-24 sm:h-32 lg:h-40  overflow-hidden rounded-md">
-              <Image
-                src="/gallery/rome-pasta.jpeg"
-                alt="rome-pasta"
-                className="w-full h-full object-cover"
-                width={500}
-                height={500}
-              />
-            </div>
-            <div className="col-span-3 h-24 sm:h-32 lg:h-40  overflow-hidden rounded-md">
-              <Image
-                src="/gallery/republique.jpeg"
-                alt="republique"
-                className="w-full h-full object-cover"
-                width={500}
-                height={500}
-              />
-            </div>
-            <div className="col-span-3 h-24 sm:h-32 lg:h-40  overflow-hidden rounded-md">
-              <Image
-                src="/gallery/nubiani.jpeg"
-                alt="nubiani"
-                className="w-full h-full object-cover"
-                width={500}
-                height={500}
-              />
-            </div>
-            <div className="col-span-3 h-24 sm:h-32 lg:h-40  overflow-hidden rounded-md">
-              <Image
-                src="/gallery/lindustrie.jpeg"
-                alt="lindustrie"
-                className="w-full h-full object-cover"
-                width={500}
-                height={500}
-              />
-            </div>
+            {galleryImages.map((image) => (
+              <div
+                key={image.alt}
+                className={`col-span-${image.colSpan} h-24 sm:h-32 lg:h-40 overflow-hidden rounded-md`}
+              >
+                <Image
+                  src={image.src}
+                  alt={image.alt}
+                  className="w-full h-full object-cover"
+                  width={500}
+                  height={500}
+                />
+              </div>
+            ))}
           </div>
         </div>
       </div>
